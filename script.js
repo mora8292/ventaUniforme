f962d668554f3417a12471ff78b47cc9120b5f6a
CesarRojasM28 <23030414@itesa.edu.mx>
2026-02-13 22:24:35 -0600

    Primer Avance Versión 0.1

--- script.js
+++ script.js
@@ -1,15 +1,6 @@
-const boton = document.getElementById("menuBtn");
-const menu = document.getElementById("menuOpciones");
+const menuToggle = document.getElementById("menuToggle");
+const sidebar = document.getElementById("sidebar");
 
-boton.addEventListener("click", (e) => {
-    e.stopPropagation();
-    menu.classList.toggle("activo");
-    boton.classList.toggle("activo");
-});
-
-document.addEventListener("click", (e) => {
-    if (!e.target.closest(".menu-contenedor")) {
-        menu.classList.remove("activo");
-        boton.classList.remove("activo");
-    }
+menuToggle.addEventListener("click", () => {
+    sidebar.classList.toggle("activa");
 });
