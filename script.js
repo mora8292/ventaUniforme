45182fed9f02d9f55c7fd0223c80ff3624c8c4bd
CesarRojasM28 <23030414@itesa.edu.mx>
2026-02-18 13:29:15 -0600

    Primer Avance Versión 0.3

--- script.js
+++ script.js
@@ -4,3 +4,15 @@ const sidebar = document.getElementById("sidebar");
 menuToggle.addEventListener("click", () => {
     sidebar.classList.toggle("activa");
 });
+
+/* Script Ventas*/
+const tallas = document.querySelectorAll(".talla");
+
+tallas.forEach(boton => {
+    boton.addEventListener("click", () => {
+
+        tallas.forEach(b => b.classList.remove("activa"));
+        boton.classList.add("activa");
+
+    });
+});
